e37e0b1f1d2330cd632f49b0c5b877daef85a6ec
wata516 <k2-watanabe@nbsi.jp>
2013-08-26 13:45:42 +0900

    Update.

--- Test/source/main.cpp
+++ Test/source/main.cpp
@@ -1,37 +1,11 @@
 #include <gtest/gtest.h>
-
-#include <Infinity/TaskManager.h>
-#include <Infinity/Task.h>
-
-#include <Infinity/Actor.h>
-
-TEST(AddTest, Test1 )
-{
-	class TestTask : public Infinity::Task
-	{
-		int mValue;
-		int mMax;
-	public:
-		TestTask(int max) : mValue(0), mMax(max) {}
-		virtual void Tick() {
-			mValue += mMax;
-		}
-	};
-	Infinity::TaskManager taskman(10);
-
-	for( int i = 0 ; i < 100 ; ++ i ) {
-		taskman.AddBack( Infinity::TaskManager::GROUP_PARALLEL, new TestTask(i) );
-	}
-	taskman.Tick();
-
-	taskman.Destroy();
-
-	ASSERT_EQ(2, 2);
-}
+#include <conio.h>
 
 GTEST_API_ int main(int argc, char **argv) {
 	std::cout << "Running main() from gtest_main.cc\n";
 
 	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	RUN_ALL_TESTS();
+
+	_getch();
 }
